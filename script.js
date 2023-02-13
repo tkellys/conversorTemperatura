43a655debb8cb4760e4819094a25cd56c5052e45
tkellys <thamireskellysantos@gmail.com>
2023-02-13 20:04:38 -0300

    segundaparte

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 function convertFahrenheit() {
-    let celsius = document.getElementById('celsius').value;
+    let celsius = document.getElementById('temp').value;
    
 // replace = trocar um item por outro, nesse caso vai trocar o '.' pela ','
    let fahrenheit = (1.8 * celsius + 32).toFixed(2).replace('.' , ',');
  
-
-    let textoresultado = 'A temperatura ' + celsius + ' °C é: ' + fahrenheit +' °F';
+    let textoresultado = 'A temperatura ' + celsius + ' °C para fahrenheit é: ' + fahrenheit +' F';
+    console.log(textoresultado);
     
     // aqui em baixo estou incluindo o texto dentro da tag <p> do html de forma automatica. 
     document.getElementsByTagName('p')[0].innerHTML = textoresultado;
 }
+
+// converter fahreineit para celsius
+function convertCelsius() {
+    let fahrenheit = document.getElementById('temp').value;
+   
+
+   let celsius = ((fahrenheit - 32) *5/9).toFixed(2).replace('.' , ',');
+ 
+    let textoresultado2 = 'A temperatura ' + fahrenheit + ' F para Celsius é: ' + celsius +' °C';
+    console.log(textoresultado2);
+     
+    document.getElementsByTagName('p')[0].innerHTML = textoresultado2;
+}
+
+// botao para limpar todas informações
 
 function limpaTudo() {
     document.getElementsByTagName('p')[0].innerHTML = '';
