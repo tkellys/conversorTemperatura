82848d2465ac3e4b67f5bb8a8d3afae409784bd9
tkellys <thamireskellysantos@gmail.com>
2023-02-13 21:45:23 -0300

    functions

---
+++ aulafunctions.js
@@ -0,0 +1,12 @@
+// reutilizar uma função mudando so os parametros - para nao ter que fazer + de uma função em um codigo
+
+function somar(n1,n2){
+    return n1 + n2;
+}
+console.log(somar(10,20));
+
+// 
+function transformarParaMaiuscula(texto){
+    return texto.toUpperCase();
+}
+console.log(transformarParaMaiuscula('bill gates'));
